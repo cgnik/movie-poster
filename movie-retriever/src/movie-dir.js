caf4a88b08e56990c13910988906bb111714f491
christo-king <cking@enmobile.biz>
2015-06-12 21:55:26 -0400

    interim for moving workstation

--- movie-retriever/src/movie-dir.js
+++ movie-retriever/src/movie-dir.js
@@ -1,51 +1,51 @@
-log = global['log'];
 // local movies library -- functions re: movie dir
-fs = module.require("fs");
-module.exports = {
-	MovieImageMap : function(dir) {
-		return {
-			dir : dir,
-			movieImages : {},
-			getMovieImageMap : function() {
-				// gets movie results for each file in movie dir
-				return mapMovieImageFiles(this.movieImages, fs.readdirSync(dir));
-			},
-
-		};
-	},
-};
-
-function mapMovieImageFiles(movieImages, files) {
-	files.forEach(function(fileFullName) {
-		mapMovieImageFile(movieImages, fileFullName);
-	})
-	return movieImages;
-}
-function mapMovieImageFile(movieImages, fileFullName) {
-	fileName = fileFullName.substring(0, fileFullName.lastIndexOf('.'));
-	if (fileName.trim().length < 1) {
-		// then it's a . file or we're likely not interested
-		return;
-	}
-	// sorting criteria -- what kind of thing is it
-	fileExtension = fileFullName.substring(fileFullName.lastIndexOf('.'),
-			fileFullName.length).toLowerCase();
-	if (isMovieFile(fileExtension)) {
-		// prevent overwrite
-		if (!(fileName in movieImages)) {
-			movieImages[fileName] = null;
+MovieImageMap = function(dir) {
+	log = global['log'];
+	var self = {
+		fs : require('fs'),
+		dir : dir,
+		movieImages : {},
+		getMovieImageMap : function() {
+			// gets movie results for each file in movie dir
+			return self.mapMovieImageFiles(this.movieImages, self.fs
+					.readdirSync(dir));
+		},
+		mapMovieImageFiles : function(movieImages, files) {
+			console.log(files);
+			files.forEach(function(fileFullName) {
+				self.mapMovieImageFile(movieImages, fileFullName);
+			})
+			return movieImages;
+		},
+		mapMovieImageFile : function(movieImages, fileFullName) {
+			fileName = fileFullName.substring(0, fileFullName.lastIndexOf('.'));
+			if (fileName.trim().length < 1) {
+				// then it's a . file or we're likely not interested
+				return;
+			}
+			// sorting criteria -- what kind of thing is it
+			fileExtension = fileFullName.substring(
+					fileFullName.lastIndexOf('.'), fileFullName.length)
+					.toLowerCase();
+			if (self.isMovieFile(fileExtension)) {
+				// prevent overwrite
+				if (!(fileName in movieImages)) {
+					movieImages[fileName] = null;
+				}
+			} else if (isImageFile(fileExtension)) {
+				// set the filename for the movie cover art
+				self.movieImages[fileName] = fileFullName;
+			}
+		},
+		isMovieFile : function(fileExtension) {
+			return fileExtension == ".m4v" || fileExtension == ".mkv"
+					|| fileExtension == ".mp4" || fileExtension == ".vob";
+		},
+		isImageFile : function(fileExtension) {
+			return fileExtension == ".jpg" || fileExtension == ".gif"
+					|| fileExtension == ".png";
 		}
-	} else if (isImageFile(fileExtension)) {
-		// set the filename for the movie cover art
-		movieImages[fileName] = fileFullName;
-	}
-}
-function isMovieFile(fileExtension) {
-	return fileExtension == ".m4v" || fileExtension == ".mkv"
-			|| fileExtension == ".mp4" || fileExtension == ".vob";
-}
-function isImageFile(fileExtension) {
-	return fileExtension == ".jpg" || fileExtension == ".gif"
-			|| fileExtension == ".png";
-
-}
+	};
+	return self;
+};
+module.exports = MovieImageMap(".");
--- movie-retriever/test/movie-dir-tests.js
+++ movie-retriever/test/movie-dir-tests.js
@@ -1,2 +1,35 @@
 assert = require('assert');
 
+var mockfs = function(dir) {
+	self = {
+		readdirSync : function(dir) {
+			return {
+				readdirSync : function(dir) {
+					return [ 'this.mp3', 'this.jpg', 'That.mp4', 'that.gif',
+							'other Thing.mkv', 'which.m4v', 'WHAT.vOb',
+							'bother.not' ];
+				}
+			};
+		}
+	};
+	return self;
+};
+var capture = function(dir) {
+	dir.fs = mockfs(".");
+	dir.dir = ".";
+	return dir.getMovieImageMap();
+};
+describe('MovieImageMap', function() {
+	dir = require('../src/movie-dir.js');
+	describe('#getMovieImageMap', function() {
+		it('should list movies in dir', function() {
+			assert(capture(dir) === {
+				'this' : 'this.jpg',
+				'that' : 'That.mp4',
+				'other Thing' : null,
+				'which' : null,
+				'WHAT' : null
+			});
+		})
+	})
+});
