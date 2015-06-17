29dec57d5cea607791b4ae3202b739efb8d8e27d
christo-king <cking@enmobile.biz>
2015-06-17 19:59:45 -0400

    working image fetching and tests

--- movie-retriever/src/image-fetch.js
+++ movie-retriever/src/image-fetch.js
@@ -1,4 +1,5 @@
 var log = global['log'];
+path = require('path');
 FileFetch = function(params) {
 	if (params === null) {
 		params = {};
@@ -6,23 +7,31 @@ FileFetch = function(params) {
 	var self = {
 		request : require('request'),
 		fs : require('fs'),
-		fetch : function(imageLoc, fileName) {
+		fetch : function() {
 			url = self.getUrl();
-			ext = self.getExtension(imageLoc);
-			fileDestination = self.imagePath + fileName;
-			log.always("Fetching " + fileDestination + " from " + url);
+			var fileTarget = self.getTargetFile();
+			log.always("Fetching " + fileTarget + " from " + url);
 			self.request.get(url).on('error', function(err) {
 				log.error(err)
-			}).pipe(self.fs.createWriteStream(fileDestination));
+			}).pipe(self.fs.createWriteStream(fileTarget));
+		},
+		getTargetFile : function() {
+			return path.join(path.normalize(self.imagePath), self.fileName
+					+ self.getExtension(self.imageLoc));
 		},
 		getUrl : function() {
 			return self.baseUrl + "w300" + self.imageLoc;
 		},
-		getExtension : function(fileName) {
-			dotLoc = fileName.lastIndexOf('.');
-			ext = fileName.substr(dotLoc, fileName.length - dotLoc);
-			return ext;
+		getExtension : function() {
+			var extension = '';
+			var dotLoc = self.imageLoc.lastIndexOf('.');
+			if (dotLoc >= 0) {
+				extension = self.imageLoc.substr(dotLoc, self.imageLoc.length
+						- dotLoc);
+			}
+			return extension;
 		},
+		fileName : '',
 		imagePath : '',
 		imageLoc : '',
 		baseUrl : ''
--- movie-retriever/test/image-fetch-tests.js
+++ movie-retriever/test/image-fetch-tests.js
@@ -3,10 +3,6 @@ var should = chai.should();
 global['log'] = require('../src/movie-log.js');
 global['log'].level = 'debug';
 
-fetcher = require('../src/image-fetch.js')({
-	baseUrl : 'BASE',
-	imagePath : 'IMAGEPATH'
-});
 var mockfs = function() {
 	self = {
 		createWriteStream : function(fileName) {
@@ -15,14 +11,19 @@ var mockfs = function() {
 	}
 	return self;
 }
-var mockrequest = function(fetch, callback, errorCallback) {
+var mockrequest = function(fetch, errorCallback) {
 	var success = false;
+	var url = fetcher.getUrl();
+	var targetFile = fetcher.getTargetFile();
 	self = {
 		get : function(url) {
+			console.log('get');
 			return {
 				on : function(type, callback) {
+					console.log('on');
 					return {
 						pipe : function(stream) {
+							console.log('fetch success');
 							success = true;
 						}
 					};
@@ -30,28 +31,51 @@ var mockrequest = function(fetch, callback, errorCallback) {
 			}
 		}
 	};
+	
 	fetcher.request = self;
-	fetcher.fetch("URIPART", "FILENAME");
-	callback();
+	fetcher.fetch();
 	return success;
 }
+ffetcher = function() {
+	return require('../src/image-fetch.js')({
+		baseUrl : 'BASEURL',
+		imagePath : 'IMAGE/PATH',
+		imageLoc : 'IMAGE/LOC',
+		fileName : 'FILENAME'
+	})
+};
 describe('FileFetch', function() {
-	fetcher.imageLoc = 'IMAGELOC';
 	describe('#getUrl', function() {
-		it('should assemble the url BASEw300IMAGELOC', function(done) {
-			assert.equal(fetcher.getUrl(), 'BASEw300IMAGELOC');
-			done();
+		it('should assemble the url BASEURLw300IMAGE/LOC', function() {
+			ffetcher().getUrl().should.equal('BASEURLw300IMAGE/LOC');
 		})
 	})
 	describe('#getExtension', function() {
-		it('should return ".ext" for file.ext', function(done) {
-			assert.equal(fetcher.getExtension('file.ext'), '.ext');
-			done();
+		it('should return ".ext" for file.ext', function() {
+			fetcher = ffetcher();
+			fetcher.imageLoc = 'file.ext';
+			fetcher.getExtension().should.equal('.ext');
+		})
+		it('should return "" for file', function() {
+			fetcher = ffetcher();
+			fetcher.imageLoc = 'file';
+			fetcher.getExtension().should.equal('');
 		})
 	})
-	describe('#getExtension', function() {
-		it('should return "ext" for file.ext', function(done) {
-			assert(mockrequest(fetcher, done, function() {
+	describe('#getTargetFile', function() {
+		it('should return "IMAGE/PATH/target.ext" for IMAGE/LOC, file',
+				function() {
+					fetcher = ffetcher();
+					fetcher.imageLoc = 'file.ext';
+					fetcher.imagePath = 'IMAGE/PATH';
+					fetcher.fileName = 'target';
+					fetcher.getTargetFile().should
+							.equal('IMAGE/PATH/target.ext');
+				})
+	})
+	describe('#fetch', function() {
+		it('should return success', function() {
+			assert(mockrequest(ffetcher(), function() {
 				assert(true === false);
 			}));
 		})
