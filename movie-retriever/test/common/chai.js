246bdfdf818f1c7f31b2fe4b7e0ae2905b8689ba
christo-king <cking@enmobile.biz>
2015-08-08 16:49:17 -0400

    chai/mocha testing working with globals now

---
+++ movie-retriever/test/common/chai.js
@@ -0,0 +1,4 @@
+global.chai = require ('chai');
+global.expect = global.chai.expect;
+global.TIMEOUT = 200;
+global.sinon = require('sinon');
--- movie-retriever/test/moviedb-queue-tests.js
+++ movie-retriever/test/moviedb-queue-tests.js
@@ -1,16 +1,11 @@
-var expect = require('chai').expect;
-
-var capture = function () {
-
-}
 describe('MovieDbQueue', function () {
-    queue = require('../src/moviedb-queue.js')({themoviedbKey: 'test-moviedb-key'});
-    describe('#findMovieId', function () {
+    this.queue = require('../src/moviedb-queue.js')({themoviedbKey: 'test-moviedb-key'});
+    describe('#configure', function () {
         var result = '';
         beforeEach(function () {
             setTimeout(function () {
                 result = capture(log, log.always, 'duh', 'always');
-            }, 50)
+            }, TIMEOUT)
         })
         it('should always log always', function () {
             expect(result).equal('duh');
