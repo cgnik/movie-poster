07008e2524130e4eddbd0a8eb049b544897cba1d
christo-king <cking@enmobile.biz>
2015-09-05 15:42:20 -0400

    logging level adjustment and providing for global when app running

--- movie-retriever/test/common/test-init.js
+++ movie-retriever/test/common/test-init.js
@@ -6,7 +6,7 @@ global.should = chai.should();
 global.TIMEOUT = 100;
 global.sinon = require('sinon');
 global.log = sinon.mock(require('../../src/movie-log.js'));
-global.log.level = 'debug';
+global.log.level = 'info';
 
 var sinonChai = require("sinon-chai");
 chai.should();
