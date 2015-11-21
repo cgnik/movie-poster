649ad015f0e4bd81f2f26070350214833ec8cd69
christo-king <cking@enmobile.biz>
2015-11-21 16:15:45 -0500

    added throw for enqueueing a null callable

--- movie-retriever/src/throttle.js
+++ movie-retriever/src/throttle.js
@@ -3,23 +3,23 @@
  */
 rateLimit = require("rate-limit");
 
-var Throttle = function () {
-    var self = ({
-        initialize: function (params) {
-            self.interval = 500;
-            if (params) {
-                merge(self, params);
-            }
-            self.queue = rateLimit.createQueue({
-                interval: self.interval
-            });
-            return self;
-        },
-        add: function (callable) {
-            self.queue.add(callable);
-        },
+function Throttle() {
+    this.interval = 500;
+};
+Throttle.prototype.initialize = function (params) {
+    if (params) {
+        merge(this, params);
+    }
+    this.queue = rateLimit.createQueue({
+        interval: this.interval
     });
-    return self.initialize();
+    return this;
+};
+Throttle.prototype.add = function (callable) {
+    if( typeof callable === 'undefined') {
+        throw new Error("Cannot specify a null callable to add to the queue");
+    }
+    this.queue.add(callable);
 };
 
-module.exports = Throttle();
+module.exports = new Throttle().initialize();
--- movie-retriever/test/throttle-tests.js
+++ movie-retriever/test/throttle-tests.js
@@ -1,7 +1,7 @@
 describe("Throttle", function () {
     var throttle = require('../src/throttle.js');
     throttle.initialize();
-    describe('initialize', function () {
+    describe('#initialize', function () {
         it('should initialize the queue', function () {
             expect(throttle.queue).not.be.empty;
             expect(throttle.queue).not.equal(undefined);
@@ -13,12 +13,17 @@ describe("Throttle", function () {
             throttle.initialize({interval: 200});
             expect(throttle.interval).equal(200);
         })
+    })
+    describe('#add', function () {
         it('should call my callback within the interval', function (done) {
             setTimeout(function () {
                 throttle.add(function () {
                     done();
                 });
             }, throttle.interval);
+        })
+        it('should throw when a null callable is specified', function () {
+            expect(throttle.add.bind(throttle, [null])).to.throw;
         })
     })
 })
