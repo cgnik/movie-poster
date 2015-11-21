2eb5cd18d952240da402c99b367ab05564b0de16
christo-king <cking@enmobile.biz>
2015-11-21 17:10:27 -0500

    bootstrapping for new mocha way

--- movie-retriever/test/bootstrap.js
+++ movie-retriever/test/bootstrap.js
@@ -5,7 +5,7 @@ global.assertEqual = global.chai.assertEqual;
 global.should = chai.should();
 global.TIMEOUT = 100;
 global.sinon = require('sinon');
-global.log = sinon.mock(require('../../src/movie-log.js'));
+global.log = sinon.mock(require('../src/movie-log.js'));
 global.log.level = 'info';
 
 var sinonChai = require("sinon-chai");
