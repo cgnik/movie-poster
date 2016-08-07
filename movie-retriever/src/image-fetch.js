81f6d96576def52558895028e4040ed90631eeab
Christopher King <cking@enmobile.biz>
2016-08-07 14:52:20 -0400

    converted fetch to class

--- movie-retriever/src/image-fetch.js
+++ movie-retriever/src/image-fetch.js
@@ -1,55 +1,56 @@
-ImageFetch = function (params) {
-    if (params === null) {
-        params = {};
+class ImageFetch {
+    constructor(params) {
+        if (params === null) {
+            params = {};
+        }
+        this.fileName = '';
+        this.imagePath = '';
+        this.imageLoc = '';
+        this.baseUrl = '';
+        merge(this, params);
     }
-    this.fileName = '';
-    this.imagePath = '';
-    this.imageLoc = '';
-    this.baseUrl = '';
-    merge(this, params);
-};
 
-ImageFetch.prototype.fetch = function () {
-    url = this.getUrl();
-    var fileTarget = this.getTargetFile();
-    log.info("Fetching '" + fileTarget + "' from '" + url + "'");
+    fetch() {
+        let url = this.getUrl();
+        let fileTarget = this.getTargetFile();
+        log.info("Fetching '" + fileTarget + "' from '" + url + "'");
 
-    dest = fs.createWriteStream(fileTarget);
-    http
-        .get(url, function (res) {
-            res.on('data', function (data) {
-                dest.write(data);
+        let dest = fs.createWriteStream(fileTarget);
+        http.get(url, function (res) {
+                res.on('data', function (data) {
+                    dest.write(data);
+                });
+                res.on('end', function () {
+                    log.always("Wrote " + fileTarget);
+                    dest.close();
+                });
+            })
+            .on('error', function (error) {
+                log.error(error);
             });
-            res.on('end', function () {
-                log.always("Wrote " + fileTarget);
-                dest.close();
-            });
-        })
-        .on('error', function (error) {
-            log.error(error);
-        });
-};
-
-ImageFetch.prototype.getTargetFile = function () {
-    return path.join(path.normalize(this.imagePath), this.fileName
-        + this.getExtension(this.imageLoc));
-};
-
-ImageFetch.prototype.getUrl = function () {
-    return this.baseUrl + "w300" + this.imageLoc;
-};
-
-ImageFetch.prototype.getExtension = function () {
-    var extension = '';
-    if (this.imageLoc == null || this.imageLoc === undefined) {
-        return;
     }
-    var dotLoc = this.imageLoc.lastIndexOf('.');
-    if (dotLoc >= 0) {
-        extension = this.imageLoc.substr(dotLoc, this.imageLoc.length
-            - dotLoc);
+
+    getTargetFile() {
+        return path.join(path.normalize(this.imagePath), this.fileName
+            + this.getExtension(this.imageLoc));
     }
-    return extension;
-};
+
+    getUrl() {
+        return this.baseUrl + "w300" + this.imageLoc;
+    }
+
+    getExtension() {
+        var extension = '';
+        if (this.imageLoc == null || this.imageLoc === undefined) {
+            return;
+        }
+        var dotLoc = this.imageLoc.lastIndexOf('.');
+        if (dotLoc >= 0) {
+            extension = this.imageLoc.substr(dotLoc, this.imageLoc.length
+                - dotLoc);
+        }
+        return extension;
+    }
+}
 
 module.exports = ImageFetch;
