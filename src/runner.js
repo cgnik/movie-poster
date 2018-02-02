9c5edb4ebf1b7883338f0c94819b6c07cc68c0cd
cgnik <cking@enmobilellc.com>
2018-02-01 21:53:35 -0500

    removed comments

--- src/runner.js
+++ src/runner.js
@@ -3,4 +3,3 @@ movies = require('./simplified');
 console.log(movies.files(process.cwd())
    .filter(movies.isMovie)
    .forEach(f => movies.movieImage(movies.fileparts(f)[0])))
-// movies.movieImage("Aliens");
