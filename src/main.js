7e534b7a32640c4fd6d808fe23dda3cefa1f635c
cgnik <cking@enmobilellc.com>
2018-02-05 22:35:42 -0500

    small refactor names

--- src/main.js
+++ src/main.js
@@ -5,15 +5,15 @@ const mdb = require('./simplified-mdb');
 const files = m.files(process.cwd()).filter(f => m.isMovie(f) || m.isImage(f));
 const movies = files.filter(m.isMovie).map(f => m.filename(f));
 
-const imagiate = (base, movies) => {
+const imaginate = (base, titles) => {
    console.log("Using base: " + base);
-   movies.forEach(movie => {
+   titles.forEach(movie => {
       mdb.search(movie)
          .then(titles => mdb.match(movie, titles))
          .then(data => mdb.check(base, data))
          .then(stream => m.write(stream, movie + ".jpg"))
-         .catch(console.error)
+         .catch(console.error);
    });
 };
 mdb.config()
-   .then(base => imagiate(base, movies))
+   .then(base => imaginate(base, movies))
