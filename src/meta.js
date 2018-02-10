24f24b2070c42253ce7225fb1774786387496926
cgnik <cking@enmobilellc.com>
2018-02-10 11:04:10 -0500

    added some merge tests; working on read method

--- src/meta.js
+++ src/meta.js
@@ -5,13 +5,15 @@ const MOVIE_FIELD_MAP = {
    Description: 'description',
    comment: 'description'
 };
-const update = (dst, src, map) => {
+const merge = (dst, src, map) => {
    if (dst && src && map) {
       Object.keys(map).forEach(k => dst[k] = src[map[k]] || dst[k]);
    }
    return dst;
 };
+const read = (file) => meta.read(file, {}, (e, d) => r({error: e, data: d}));
 
 module.exports = {
-   update: update
+   merge: merge,
+   read: read
 };
--- test/meta-tests.js
+++ test/meta-tests.js
@@ -2,26 +2,40 @@ let meta = rewire('../src/meta');
 
 
 describe("meta", () => {
-   describe('#update', () => {
+   describe('#merge', () => {
       it('should set values on the target from the metadata', () => {
-         let src = {derp: "dorp", flerg: "florp"};
-         let dst = {derp: "drep", gerp: "gorp"};
-         let map = {grop: "flerg"};
-         let expected = {derp: "drep", grop: "florp", gerp: "gorp"};
-         meta.update(dst, src, map).should.deep.equal(expected);
+         let src = {a: 1, b: 2};
+         let dst = {a: 3, c: 4};
+         let map = {d: "b"};
+         let expected = {a: 3, c: 4, d: 2};
+         meta.merge(dst, src, map).should.deep.equal(expected);
       });
       it('should tolerate empty arguments', () => {
-         meta.update.bind().should.not.throw;
+         meta.merge.bind().should.not.throw;
       });
       it('should give back first argument unaltered if other args are empty', () => {
          let expected = {derp: "drep", grop: "florp"};
-         meta.update(expected).should.equal(expected);
+         meta.merge(expected).should.equal(expected);
       });
       it('should not replace existing with empty', () => {
-         let src = {derp: "dorp", flerp: "florp"};
-         let dst = {derp: "drep", gerp: "gorp"};
-         let map = {flerp: null};
-
+         let src = {a: 1};
+         let dst = {a: 3, b: 5, c: 4};
+         let map = {d: "a", b: "b"};
+         let expected = {a: 3, b: 5, c: 4, d: 1};
+         meta.merge(dst, src, map).should.deep.equal(expected);
+      });
+   });
+   describe('#read', () => {
+      let ffm = null;
+      beforeEach(() => {
+         ffm = {read: sinon.stub()};
+         meta.__set__('meta', ffm);
+      });
+      it('should read the metadata from the file', () => {
+         let filename = 'file.fil';
+         let expected = {title: 'test'};
+         ffm.read.withArgs(filename).returns(expected);
+         meta.read(filename).should.equal(expected);
       });
    });
 });
